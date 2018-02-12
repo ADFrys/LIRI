804816d1b6cf700d84df554a2bf6e1242135253c
AngelaF <ADFrys@users.noreply.github.com>
2018-02-12 16:47:28 -0600

    Add spotify functionality.

--- liri.js
+++ liri.js
@@ -6,43 +6,43 @@ var request = require("request");
 var Twitter = require("twitter");
 var client = new Twitter(keys.twitter);
 
-var SpotifyApi = require('node-spotify-api');
-var spotify = new SpotifyApi(keys.spotify);
+var Spotify = require('node-spotify-api');
+var spotify = new Spotify(keys.spotify);
 
 // var random = require("./random.txt");
 
 var fs = require("fs");
 
-var userInput = process.argv;
-var command = userInput[2];
+var userInput = process.argv[2];
 
-if (command === "my-tweets") {
-client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alliasAngela', function(error, tweets, response) {
+if (userInput === "my-tweets") {
+  client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alliasAngela', function(error, tweets, response) {
 
 	if (error) {
 		console.log(error);
 	}
-
-	for (i=0; i<tweets.length; i++) {
-	console.log(tweets[i].text);
+  if (!error && response.statusCode == 200) {
+	  for (i=0; i<tweets.length; i++) {
+	    console.log(tweets[i].text);
+      }
     }
-
   });
-
-if (command === "spotify-this-song") {
-
 }
 
-}
-
-// node liri.js spotify-this-song '<song name here>'
-// This will show the following information about the song in your terminal/bash window
-
-//Artist(s)
-//The song's name
-//A preview link of the song from Spotify
-//The album that the song is from
-//If no song is provided then your program will default to "The Sign" by Ace of Base.
+if (userInput === "spotify-this-song") {
+  var songname = "Ace of Base The Sign";
+  spotify.search({ type: 'track', query: songname, limit: 1}, function(error, data, response) {
+      if (error) {
+        console.log(error);
+      }
+      if (!error) {
+        console.log("Artist: " + data.tracks.items[0].artists[0].name);
+        console.log("Song Name: " + data.tracks.items[0].name);
+        console.log("Preview link: " + data.tracks.items[0].preview_url);
+        console.log("The album is: " + data.tracks.items[0].album.name);
+      }
+    });
+  }
 
 // var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
 // request("http://www.omdbapi.com/?t=remember+the+titans&y=&plot=short&apikey=trilogy", function(error, response, body) {
