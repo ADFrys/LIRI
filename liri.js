a5846c495b4fa3474064af96631a6f7eb1dd7d7f
AngelaF <ADFrys@users.noreply.github.com>
2018-02-13 16:41:26 -0600

    Fix Do-this command and change comamnds into functions.

--- liri.js
+++ liri.js
@@ -1,4 +1,6 @@
 
+// Define variables and required packages
+
 require("dotenv").config();
 var keys = require("./keys.js");
 var request = require("request");
@@ -9,29 +11,12 @@ var client = new Twitter(keys.twitter);
 var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
-// var random = require("./random.txt");
-
 var fs = require("fs");
 
 var userInput = process.argv[2];
 
-if (userInput === "my-tweets") {
-  client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alliasAngela', function(error, tweets, response) {
-
-	if (error) {
-		console.log(error);
-	}
-  if (!error && response.statusCode === 200) {
-	  for (i=0; i<tweets.length; i++) {
-	    console.log(tweets[i].text);
-      }
-    }
-  });
-}
-
-if (userInput === "spotify-this-song") {
-  var songTitleInput = process.argv;
-  var songTitle = "";
+// Define function for obtaining song information 
+var songInformation = function (songTitleInput, songTitle) {
   for (var j = 3; j < songTitleInput.length; j++) {
 
   if (j > 3 && j < songTitleInput.length) {
@@ -40,25 +25,25 @@ if (userInput === "spotify-this-song") {
   else {
     songTitle += songTitleInput[j];
   }
+}  
+
+if (songTitle === "") {
+songTitle = "Ace of Base The Sign";
+}
+spotify.search({ type: 'track', query: songTitle, limit: 1}, function(error, data, response) {
+  if (error) {
+    console.log(error);
+  }
+    console.log("Artist: " + data.tracks.items[0].artists[0].name);
+    console.log("Song Name: " + data.tracks.items[0].name);
+    console.log("Preview link: " + data.tracks.items[0].preview_url);
+    console.log("The album is: " + data.tracks.items[0].album.name);
+});
 }
 
-  if (songTitle === "") {
-    songTitle = "Ace of Base The Sign";
-  }
-  spotify.search({ type: 'track', query: songTitle, limit: 1}, function(error, data, response) {
-      if (error) {
-        console.log(error);
-      }
-        console.log("Artist: " + data.tracks.items[0].artists[0].name);
-        console.log("Song Name: " + data.tracks.items[0].name);
-        console.log("Preview link: " + data.tracks.items[0].preview_url);
-        console.log("The album is: " + data.tracks.items[0].album.name);
-    });
-  }
+// Define function for obtaining movie information
 
-if (userInput === "movie-this") {
-  var movieInput = process.argv;
-  var movieTitle = "";
+var movieInformation = function (movieInput, movieTitle) {
   for (var k = 3; k < movieInput.length; k++) {
 
   if (k > 3 && k < movieInput.length) {
@@ -84,29 +69,71 @@ if (movieTitle === "") {
       console.log("The plot is: " + JSON.parse(body).Plot);
       console.log("The actors are: " + JSON.parse(body).Actors);
     }
+    return error;
   });
 }
 
-// if (userInput === "do-what-it-says") {
-//   fs.readFile("random.txt", "utf8", function(error, data) {
+//Define function for tweets
 
-//   if (error) {
-//     return console.log(error);
-//   }
-//   var dataArray = data.split(",");
-//   if (dataArray[0] === "spotify-this-song") {
-//   	console.log(dataArray[1]);
-//   }
-//   if (dataArray[0] === "movie-this") {
-//   	console.log(dataArray[1]);
+var obtainTweets = function () {
+  client.get('https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=alliasAngela', function(error, tweets, response) {
 
-//   }
+    if (error) {
+	  console.log(error);
+	  }
+    if (!error && response.statusCode === 200) {
+	  for (i=0; i<tweets.length; i++) {
+	  console.log(tweets[i].text);
+      }
+    }
+  });
+}
+
+// If-statement logic for LIRI text commands
+
+if (userInput === "my-tweets") {
+	obtainTweets();
+
+}
+
+if (userInput === "spotify-this-song") {
+  var songTitleInput = process.argv;
+  var songTitle = "";
+  songInformation(songTitleInput, songTitle);
+  }
 
 
-//   })
+if (userInput === "movie-this") {
+  var movieInput = process.argv;
+  var movieTitle = "";
+  movieInformation(movieInput, movieTitle);
+}
 
+// Do what it says logic
 
-// }
+if (userInput === "do-what-it-says") {
+  fs.readFile("random.txt", "utf8", function(error, data) {
 
+  if (error) {
+    return console.log(error);
+  }
 
+  var dataArray = data.split(",");
 
+  if (dataArray[0] === "spotify-this-song") {
+    var songTitleInput = dataArray[1];
+    var songTitle = "";
+    songInformation(songTitleInput, songTitle);
+    }
+
+  if (dataArray[0] === "movie-this") {
+  	var movieInput = dataArray[1];
+  	var movieTitle = "";
+    movieInformation(movieInput, movieTitle);
+    }
+
+  if (dataArray[0] ==="my-tweets") {
+    obtainTweets();
+    }
+  });
+};
