2aa73677806bc6d8c5011994a53683f5780ee834
AngelaF <ADFrys@users.noreply.github.com>
2018-02-15 13:46:22 -0600

    Add validation logic.

--- liri.js
+++ liri.js
@@ -45,14 +45,15 @@ if (movieTitle === "") {
   var movieURL = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey=trilogy";
   request(movieURL, function (error, response, body) {
     if (!error && response.statusCode ===200) {
+      console.log(response);
       console.log("The movie title is: " + JSON.parse(body).Title);
       console.log("The year the movie came out is " + JSON.parse(body).Year);
       console.log("The IMDB Rating of the movie is " + JSON.parse(body).imdbRating);
-      console.log("The Rotten Tomatoes Rating of the movie is " + JSON.parse(body).Ratings[1].Value);
       console.log("The country/countries where the movie was produced is: " + JSON.parse(body).Country);
       console.log("The language(s) of the movie is/are: " + JSON.parse(body).Language);
       console.log("The plot is: " + JSON.parse(body).Plot);
       console.log("The actors are: " + JSON.parse(body).Actors);
+      console.log("The Rotten Tomatoes Rating of the movie is " + JSON.parse(body).Ratings[1].Value);
     }
     else {
       return error;
@@ -81,99 +82,100 @@ function invalidInput() {
   console.log("This is not a valid LIRI command. Please enter one of the following commands: my-tweets, spotify-this-song song name, movie-this movie name, do-what-it-says. For example, 'node liri.js movie-this The Wizard of Oz'");
 }
 
-// If-statement logic stored in a function for LIRI text commands
+// Define function for do-what-it-says
 
-function runLiri() {
+function doWhatSays() {
+  fs.readFile("random.txt", "utf8", function(error, data) {
 
-  if (userInput === "my-tweets") {
-    obtainTweets();
-    }
+  if (error) {
+    console.log(error);
+    return;
+  }
 
-  if (userInput === "spotify-this-song") {
-    var songTitleInput = process.argv;
-    var songTitle = "";
-    for (var j = 3; j < songTitleInput.length; j++) {
+  else {
+    var dataArray = data.split(",");
 
-      if (j > 3 && j < songTitleInput.length) {
-        songTitle = songTitle + "+" + songTitleInput[j];
+    if (dataArray[0] === "spotify-this-song") {
+      var songTitleInput = dataArray[1];
+      var songTitle = "";
+      for (var j = 0; j < songTitleInput.length; j++) {
+
+        if (j > 0 && j < songTitleInput.length) {
+          songTitle = songTitle + songTitleInput[j];
         }
-      else {
-        songTitle += songTitleInput[j];
+        else {
+          songTitle += songTitleInput[j];
         }
-      }   
+      }  
     songInformation(songTitleInput, songTitle);
-    console.log(songTitle);
-  }
-
-
-  if (userInput === "movie-this") {
-    var movieInput = process.argv;
-    var movieTitle = "";
-    for (var k = 3; k < movieInput.length; k++) {
-
-      if (k > 3 && k < movieInput.length) {
-        movieTitle = movieTitle + "+" + movieInput[k];
-      }
-      else {
-        movieTitle += movieInput[k];
-      }
     }
 
-    console.log(movieTitle);
-    movieInformation(movieInput, movieTitle);
-  }
+    if (dataArray[0] === "movie-this") {
+	  var movieInput = dataArray[1];
+	  var movieTitle = "";
+      for (var k = 0; k < movieInput.length; k++) {
 
-// Do what it says logic
-
-  if (userInput === "do-what-it-says") {
-    fs.readFile("random.txt", "utf8", function(error, data) {
-
-    if (error) {
-      return console.log(error);
+        if (k > 0 && k < movieInput.length) {
+          movieTitle = movieTitle + movieInput[k];
+        }
+        else {
+          movieTitle += movieInput[k];
+          }
+        }
+      movieInformation(movieInput, movieTitle);
+      }
+  
+    if (dataArray[0] ==="my-tweets") {
+      obtainTweets();
+      }
     }
+  });
+}
 
+// If-else statement logic for LIRI text commands
+
+if (userInput === "my-tweets") {
+  obtainTweets();
+  return;
+}
+
+if (userInput === "spotify-this-song") {
+  var songTitleInput = process.argv;
+  var songTitle = "";
+  for (var j = 3; j < songTitleInput.length; j++) {
+
+    if (j > 3 && j < songTitleInput.length) {
+      songTitle = songTitle + "+" + songTitleInput[j];
+    }
     else {
-      var dataArray = data.split(",");
+      songTitle += songTitleInput[j];
+    }
+  }   
+  songInformation(songTitleInput, songTitle);
+  return;
+}
 
-      if (dataArray[0] === "spotify-this-song") {
-        var songTitleInput = dataArray[1];
-        var songTitle = "";
-        for (var j = 0; j < songTitleInput.length; j++) {
+if (userInput === "movie-this") {
+  var movieInput = process.argv;
+  var movieTitle = "";
+  for (var k = 3; k < movieInput.length; k++) {
 
-          if (j > 0 && j < songTitleInput.length) {
-            songTitle = songTitle + songTitleInput[j];
-          }
-          else {
-            songTitle += songTitleInput[j];
-          }
-        }  
-        console.log(songTitle);
-        songInformation(songTitleInput, songTitle);
+    if (k > 3 && k < movieInput.length) {
+      movieTitle = movieTitle + "+" + movieInput[k];
       }
-
-        if (dataArray[0] === "movie-this") {
-  	      var movieInput = dataArray[1];
-  	      var movieTitle = "";
-          for (var k = 0; k < movieInput.length; k++) {
-
-            if (k > 0 && k < movieInput.length) {
-              movieTitle = movieTitle + movieInput[k];
-            }
-            else {
-              movieTitle += movieInput[k];
-            }
-          }
-        }
-      
-        if (dataArray[0] ==="my-tweets") {
-          obtainTweets();
-        }
-      }
-    })
+    else {
+      movieTitle += movieInput[k];
+    }
   }
-  // else {
-  //   invalidInput();  	
-  // }
-};
+  movieInformation(movieInput, movieTitle);
+  return;
+}
 
-runLiri();
+if (userInput === "do-what-it-says") {
+  doWhatSays();
+  return;
+}
+
+else {
+  invalidInput(); 
+}
